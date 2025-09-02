b84d75320aac42cb25de2154f162e13cdac704ee
TheWeaver0 <weaver0thedaem0n@gmail.com>
2025-09-02 15:01:18 +0100

    testing decryption

---
+++ caesar.py
@@ -0,0 +1,44 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+def caesar(message, offset, direction = 1):
+    final_message = ''
+    for char in message.lower():
+        if not char.isalpha():
+            final_message += char
+        else:
+            idx = alphabet.index(char)
+            new_char = alphabet[(idx + offset*direction) % len(alphabet)]
+            final_message += new_char
+    return final_message
+
+def encrypt(message, offset):
+    return caesar(message, offset)
+
+def decrypt(message, offset):
+    return caesar(message, offset, -1)
+test_cases= {
+    "HELLO": [3, "khoor"],
+    "Python": [5, "udymts"],
+    "abc": [2, "cde"],
+    "Zoo": [1, "app"],
+    "OpenAI": [13, "bcranv"],
+    "Cipher Test": [7, "jpwoly alza"],
+    "Data Science": [4, "hexe wgmirgi"],
+    "Secret123": [10, "combod123"],
+    "Good Morning": [8, "owwl uwzvqvo"],
+    "Attack at Dawn": [25, "zsszbj zs czvm"]
+}
+
+
+
+def main():
+
+    for key, value in test_cases.items():
+        encryption = encrypt(key, value[0])
+        print(f'Encryption process: message: {key}, key & final_message: {value}, Encryption: {encryption}, Comparaison: {encryption == value[1]} \n')
+        decryption = decrypt(encryption, value[0])
+        print(f'Decryption process: key & final_message:: {value}, message: {key.lower()}, Decryption: {decryption}, Comparaison: {decryption == key.lower()} \n')
+
+    # encryption = encrypt("HELLO", test_cases["HELLO"][0])
+    # print(encryption)
+if __name__ == "__main__":
+    main()
